5e721eb2046a9e5f6ad20a52a751419fa8050bef
soonma <clsrn77990@naver.com>
2024-07-21 00:37:34 +0900

    del : 테스트용으로 생성한 Entity 삭제 및 코드 리뷰 적용2

--- src/main/java/com/sparta/itsmine/domain/qna/dto/QnaRequestDto.java
+++ src/main/java/com/sparta/itsmine/domain/qna/dto/QnaRequestDto.java
@@ -1,15 +1,15 @@
 package com.sparta.itsmine.domain.qna.dto;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class QnaRequestDto {
 
-    @NotNull(message = "제목이 비어있습니다.")
+    @NotBlank(message = "제목이 비어있습니다.")
     private String title;
 
-    @NotNull(message = "본문의 비어있습니다.")
+    @NotBlank(message = "본문의 비어있습니다.")
     private String content;
 
     private boolean secretQna = false;
