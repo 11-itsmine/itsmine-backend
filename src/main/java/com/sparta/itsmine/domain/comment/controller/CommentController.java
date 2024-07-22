b12444080cfe65deb0922d7104c0f97721eb23dc
JangHyun <yugi82828@naver.com>
2024-07-22 10:07:22 +0900

    udate : 댓글 삭제 기능 구현 [#8] refactor : 댓글 기능 전체적인 코드리뷰 기반 리펙터링

--- src/main/java/com/sparta/itsmine/domain/comment/controller/CommentController.java
+++ src/main/java/com/sparta/itsmine/domain/comment/controller/CommentController.java
@@ -12,7 +12,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-@Slf4j
 @RestController
 @RequestMapping("/qnas/{qnaId}/comments")
 @RequiredArgsConstructor
