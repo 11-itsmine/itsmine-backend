808e7598d2a28c52bbab4e984bc1a6d7de12a8c3
leebeanbin <wjdqlsdy1004@naver.com>
2024-08-01 11:07:13 +0900

    refactor : 최고사 입력시 바로 메세지 쏘기

--- src/main/java/com/sparta/itsmine/domain/product/scheduler/MessageListener.java
+++ src/main/java/com/sparta/itsmine/domain/product/scheduler/MessageListener.java
@@ -3,10 +3,12 @@ package com.sparta.itsmine.domain.product.scheduler;
 import static com.sparta.itsmine.domain.product.utils.ProductStatus.FAIL_BID;
 import static com.sparta.itsmine.domain.product.utils.ProductStatus.SUCCESS_BID;
 import static com.sparta.itsmine.global.common.config.RabbitConfig.PRODUCT_QUEUE_NAME;
+import static com.sparta.itsmine.global.common.response.ResponseExceptionEnum.PRODUCT_NOT_FOUND;
 
 import com.sparta.itsmine.domain.auction.repository.AuctionRepository;
 import com.sparta.itsmine.domain.product.entity.Product;
 import com.sparta.itsmine.domain.product.repository.ProductRepository;
+import com.sparta.itsmine.global.exception.DataNotFoundException;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,19 +26,15 @@ public class MessageListener {
     @RabbitListener(queues = PRODUCT_QUEUE_NAME)
     public void receiveMessage(Long productId) {
         Product product = productRepository.findById(productId)
-                .orElseThrow(() -> new RuntimeException("Product not found"));
+                .orElseThrow(() -> new DataNotFoundException(PRODUCT_NOT_FOUND));
 
-        // 입찰 기록 확인 - ProductId를 통해서 입찰 테이블에 기록된것을 찾습니다.
-        boolean existAutction = auctionRepository.findAllByProductId(productId).isEmpty();
-        // 상태가 만료 상태인지 확인
-        if (product.getDueDate().isBefore(LocalDateTime.now())) {
-            product.updateStatus(SUCCESS_BID);
+        boolean hasNoAuctions = auctionRepository.findAllByProductId(productId).isEmpty();
+
+        if (product.getDueDate().isBefore(LocalDateTime.now()) || product.getAuctionNowPrice()
+                .equals(product.getCurrentPrice())) {
+            product.updateStatus(hasNoAuctions ? FAIL_BID : SUCCESS_BID);
             productRepository.save(product);
-            log.info("Product with ID {} has been succeed as {}.", productId, SUCCESS_BID);
-        } else if (product.getDueDate().isBefore(LocalDateTime.now()) && existAutction) {
-            product.updateStatus(FAIL_BID);
-            productRepository.save(product);
-            log.info("Product with ID {} has been marked as {}.", productId, FAIL_BID);
+            log.info("Product with ID {} has been marked as {}.", productId, product.getStatus());
         } else {
             log.info("Product with ID {} is still valid.", productId);
         }
