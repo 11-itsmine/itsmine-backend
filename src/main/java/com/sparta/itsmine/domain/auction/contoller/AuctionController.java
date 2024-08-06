67fe052ccc6238eb7df09223cc28159e8d6bcb9a
JangHyun <yugi82828@naver.com>
2024-08-06 16:28:04 +0900

    update : api /v1 수정

--- src/main/java/com/sparta/itsmine/domain/auction/contoller/AuctionController.java
+++ src/main/java/com/sparta/itsmine/domain/auction/contoller/AuctionController.java
@@ -17,13 +17,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/v1")
 @RequiredArgsConstructor
 public class AuctionController {
 
