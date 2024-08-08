51b18d03d57329205b374f7c0609d808751d2eff
JangHyun <yugi82828@naver.com>
2024-08-08 15:54:28 +0900

    refactor : host, port 설정

--- src/main/java/com/sparta/itsmine/global/common/config/RedisConfig.java
+++ src/main/java/com/sparta/itsmine/global/common/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.sparta.itsmine.global.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -11,9 +12,18 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.data.redis.host}")
+    public String host;
+
+    @Value("${spring.data.redis.port}")
+    public int port;
+
+    private static final String REDISSON_HOST_PREFIX = "redis://";
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+
+        return new LettuceConnectionFactory(host,port);
     }
 
     @Bean
