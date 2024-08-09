c6cd8440dd0444d2b0c4a8f4cdf68e90a91c94c3
soonma <clsrn77990@naver.com>
2024-08-10 00:44:59 +0900

    add : 재 병합

--- frontend/src/main/Main.js
+++ frontend/src/main/Main.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 import styled from 'styled-components';
 import Carousel from "../components/carousel/Carousel";
 import ItemList from "../components/item/ItemList";
-import Board from "../components/chat/Board"
+import Board from "../components/chat/Board";
 import axiosInstance from '../api/axiosInstance'; // Axios 인스턴스 가져오기
 import Nav from "../components/nav/Nav"; // Nav 컴포넌트 가져오기
 
@@ -12,7 +12,7 @@ function Main() {
   const [userRole, setUserRole] = useState(''); // 사용자 역할 상태 추가
 
   useEffect(() => {
-    // Board에 필요한 데이터 가져오기
+    // 채팅방 데이터 가져오기
     axiosInstance.get('/v1/chatrooms')
     .then(response => {
       setChatRooms(response.data);
@@ -55,12 +55,11 @@ function Main() {
           {userRole === 'MANAGER' && (
               <div>
                 <button
-                    onClick={() => window.location.href = '/reported-users'}>신고된
-                  유저 목록
+                    onClick={() => window.location.href = '/reported-users'}>
+                  신고된 유저 목록
                 </button>
-                <button
-                    onClick={() => window.location.href = '/banned-users'}>유저
-                  리스트
+                <button onClick={() => window.location.href = '/banned-users'}>
+                  유저 리스트
                 </button>
               </div>
           )}
