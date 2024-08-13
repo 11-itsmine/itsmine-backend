63936d25bffaa0dc81c09124fd60bd698f53a1aa
soonma <clsrn77990@naver.com>
2024-08-14 00:23:54 +0900

    refactor : block 된 유저 채팅창 비활성화

--- src/main/java/com/sparta/itsmine/global/common/config/WebSocketConfig.java
+++ src/main/java/com/sparta/itsmine/global/common/config/WebSocketConfig.java
@@ -3,7 +3,6 @@ package com.sparta.itsmine.global.common.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -45,8 +44,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         // 주소 : ws://localhost:8080/ws
     }
 
-    @Override
-    public void configureClientInboundChannel(ChannelRegistration registration) {
-        registration.interceptors(chatMessageInterceptor);
-    }
+//    @Override
+//    public void configureClientInboundChannel(ChannelRegistration registration) {
+//        registration.interceptors(chatMessageInterceptor);
+//    }
 }
