5b2c647197cfab5c011617ab0187fe31ee7b0659
leebeanbin <wjdqlsdy1004@naver.com>
2024-08-21 20:48:58 +0900

    refactor : 카테고리 주입 코드 추가

--- src/main/java/com/sparta/itsmine/domain/category/CategoryInitializer.java
+++ src/main/java/com/sparta/itsmine/domain/category/CategoryInitializer.java
@@ -1,25 +1,27 @@
 package com.sparta.itsmine.domain.category;
 
 import com.sparta.itsmine.domain.category.dto.CategoryDto;
+import com.sparta.itsmine.domain.category.repository.CategoryRepository;
 import com.sparta.itsmine.domain.category.service.CategoryService;
 import java.util.Arrays;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class CategoryInitializer implements CommandLineRunner {
 
     private final CategoryService categoryService;
-
-    @Autowired
-    public CategoryInitializer(CategoryService categoryService) {
-        this.categoryService = categoryService;
-    }
+    private final CategoryRepository categoryRepository;
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         // 추가하고자 하는 카테고리 목록을 리스트로 정의
         List<String> categories = Arrays.asList(
                 "주방용품",
@@ -36,8 +38,15 @@ public class CategoryInitializer implements CommandLineRunner {
         CategoryDto categoryDto = new CategoryDto();
 
         // 반복문을 통해 카테고리 생성
-        categories.forEach(categoryName ->
-                categoryService.createCategory(categoryDto.addCategoryName(categoryName))
-        );
+        categories.forEach(categoryName -> {
+            try {
+                // 중복 생성 방지 로직 추가
+                if (!categoryRepository.existsByCategoryName(categoryName)) {
+                    categoryService.createCategory(categoryDto.addCategoryName(categoryName));
+                }
+            } catch (Exception e) {
+                log.error("Failed to create category '{}': {}", categoryName, e.getMessage());
+            }
+        });
     }
 }
